9c8f1632d1c613a4137007a8ba90ea452e4e9caf
Korkmatik <fahri.korkmaz@protonmail.ch>
2019-12-23 01:25:10 +0100

    Added functionality to specify the port; Http header will be parsed now

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,12 +1,17 @@
 #include "Server.hpp"
 
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
 #include <cstdio>
 #include <cstring>
 #include <unistd.h>
 
 #include "ServerError.hpp"
 
-Server::Server(int port)
+Server::Server(int port, int maxConnections)
 {
 	auto addressFamily = AF_INET;
 	auto serviceType = SOCK_STREAM;
@@ -15,8 +20,8 @@ Server::Server(int port)
 
     socket_fd = socket(addressFamily, serviceType, 0);
     if (socket_fd == -1) {
-		auto error = ServerCreateSocketError();
-        throwError(error);
+		cleanUp();
+		throw ServerCreateSocketError();
     }
 
 	printf("Created socket (FD #%d)\n", socket_fd);
@@ -30,11 +35,18 @@ Server::Server(int port)
 	int bindReturnValue = bind(socket_fd, (struct sockaddr *)sock_addr, sizeof(*sock_addr));
 
 	if (bindReturnValue < 0) {
-		auto error = ServerBindError();
-		throwError(error);
+		cleanUp();
+		throw ServerBindError();
 	}
 
 	printf("Socket was bind on port: %d\n", this->port);
+
+	if (listen(socket_fd, maxConnections) < 0) {
+		cleanUp();
+		throw ServerListenError();
+	}
+
+	printf("Server is listening\n");
 }
 
 Server::~Server()
@@ -42,21 +54,15 @@ Server::~Server()
 	cleanUp();
 }
 
-void Server::run(int maxConnections)
+void Server::run()
 {
-	if (listen(socket_fd, maxConnections) < 0) {
-		auto error = ServerListenError();
-		throwError(error);
-	}
-
-	printf("Server is listening\n");
 
 	int incoming_connection_fd;
 	sockaddr_in* incoming_sockaddress = new sockaddr_in;
 	int addr_len = sizeof(*incoming_sockaddress);
-	if ((incoming_connection_fd = accept(socket_fd, (struct sockaddr *)incoming_sockaddress, (socklen_t*)&incoming_sockaddress)) < 0) {
-		auto error = ServerAcceptError();
-		throwError(error);
+	if ((incoming_connection_fd = accept(socket_fd, (struct sockaddr *)incoming_sockaddress, (socklen_t*)&addr_len)) < 0) {
+		cleanUp();
+		throw ServerAcceptError();
 	}
 
 	printf("Accepted connection from client\n");
@@ -72,25 +78,63 @@ void Server::cleanUp()
     }
 }
 
-void Server::throwError(std::exception e)
-{
-	cleanUp();
-	throw e;
-}
-
 void Server::sendData(const int& to_fd, const sockaddr_in* to_addr) const
 {
-	char buffer[1024] = {0};
-
-	int valread = read(to_fd, buffer, 1024);
-	printf("%s\n", buffer);
+	std::cout << "Connection from: " << ntohl(to_addr->sin_addr.s_addr) << "\n";
 	
-	if (valread < 0) {
-		printf("No bytes are there to read\n");
+	int buffer_size = 4096;
+	char* client_message = new char[buffer_size];
+	int read_size = 0;
+
+	printf("Client message:\n");
+	std::stringstream http_header; 
+
+	std::string http_method;
+	std::string path;
+	std::string protocol;
+
+	read_size = recv(to_fd, client_message, buffer_size, 0);
+	if (read_size > 0) {
+		printf("%s", client_message);
+
+		http_header << std::string(client_message);
+	} 
+	else {
+		printf("Client hasn't send any message\n");
 		return;
 	}
 
-	char * hello = "Hello from the server";
+	std::vector<std::string> lines;
+	std::string segment;
+	while(std::getline(http_header, segment)) {
+		lines.push_back(segment);
+	}
+
+	try {
+		// reading first line
+		for (int i = 0; i < 3; ++i) {
+			http_method += lines[0].at(i);
+		}
+
+		int i = 4;
+		while (lines[0].at(i) != ' ') {
+			path += lines[0].at(i);
+			i += 1;
+		}
+
+		i += 1;
+		for(; i < lines[0].length(); i++) {
+			protocol += lines[0].at(i);
+		}
+
+		std::cout << "HTTP Method: " << http_method << "\n";
+		std::cout << "Path: " << path << "\n";
+		std::cout << "Protocol: " << protocol << "\n\n";
+	} catch (std::exception& e) {
+		printf("Not a HTTP Request\n");
+	}
+
+	char hello[] = "Hello from the server";
 	write(to_fd, hello, strlen(hello));
 
 	close(to_fd);
